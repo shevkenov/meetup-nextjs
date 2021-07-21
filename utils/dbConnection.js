be4ec120a5752c4507dd06076f59bbaf2952faf2
Andrew Shevkenov <shevkenov@users.noreply.github.com>
2021-07-21 12:34:37 +0300

    Update dbConnection.js

--- utils/dbConnection.js
+++ utils/dbConnection.js
@@ -1,7 +1,7 @@
 import { MongoClient } from "mongodb";
 
 export default async function dbConnection() {
-    const client = await MongoClient.connect('mongodb+srv://user:user123456@cluster0.vxynr.mongodb.net/meetup?retryWrites=true&w=majority');
+    const client = await MongoClient.connect('mongodb+srv://user:______@cluster0.vxynr.mongodb.net/meetup?retryWrites=true&w=majority');
     const db = client.db();
     const meetupsCollections = db.collection('meetups');
 
